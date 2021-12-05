17e6c515649cc82be39a7fa20fec1202a4dfc4bc
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2021-12-05 16:40:36 -0700

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(meal) {
+  return `Delicious ${meal}`;
 }
 
+function createMenuItem(mealName, mealPrice, mealType) {
+  var menuDescription = {
+    name: mealName,
+    price: mealPrice,
+    type: mealType,
+  }
+  return menuDescription;
+}
+
+function addIngredients(item, array) {
+  if (array.includes(item)) {
+    return array;
+  }
+  array.push(item);
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price * 0.90;
+}
+
+function createRecipe(titleName, ingredientsName, typeName) {
+  var potato = {
+    title: titleName,
+    ingredients: ingredientsName,
+    type: typeName,
+  }
+  return potato
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
